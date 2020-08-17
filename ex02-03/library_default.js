4a8183e2496877486a29ae1f831ecd0db41c91a7
HyeonJu <hj950000@ajou.ac.kr>
2020-08-17 17:30:24 +0900

    first commit

---
+++ ex02-03/library_default.js
@@ -0,0 +1,3 @@
+// 기본으로 내보내기
+const fnPlusNumbers = (pNum1, pNum2) => pNum1 + pNum2;
+export default fnPlusNumbers;
---
+++ ex02-03/library_named.js
@@ -0,0 +1,8 @@
+// 이름으로 내보내기
+const conHello = "안녕하세요!";
+const fnPlusNumbers = (pNum1, pNum2) => pNum1 + pNum2;
+
+export { conHello, fnPlusNumbers };
+
+// export const conHello = "안녕하세요!";
+// export const fnPlusNumbers = (pNum1, pNum2) => pNum1 + pNum2;
